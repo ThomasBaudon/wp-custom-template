ec33f379b57d04a45d04c58741ff0c7e5ba5a180
ThomasBaudon <tbaudon@yahoo.fr>
2022-11-10 15:27:11 +0100

    "Ajout Portfolio et gestion taxonomies"

--- functions.php
+++ functions.php
@@ -27,6 +27,7 @@ remove_action( 'wp_print_styles', 'print_emoji_styles' );
 remove_action( 'admin_print_styles', 'print_emoji_styles' );
 
 /* FUNCTIONS */
+
 function additional_custom_styles() {
 
     /*Enqueue The Styles*/
@@ -34,8 +35,52 @@ function additional_custom_styles() {
 }
 add_action( 'wp_enqueue_scripts', 'additional_custom_styles' );
 
+
 function thomas_remove_menu_pages() {
 	remove_menu_page( 'tools.php' );
     remove_menu_page( 'edit-comments.php' );
 }
 add_action( 'admin_menu', 'thomas_remove_menu_pages' );
+
+
+function thomas_register_post_types() {
+	
+    // CPT Portfolio
+    $labels = array(
+        'name' => 'Portfolio',
+        'all_items' => 'Tous les projets',  // affiché dans le sous menu
+        'singular_name' => 'Projet',
+        'add_new_item' => 'Ajouter un projet',
+        'edit_item' => 'Modifier le projet',
+        'menu_name' => 'Portfolio'
+    );
+
+	$args = array(
+        'labels' => $labels,
+        'public' => true,
+        'show_in_rest' => true,
+        'has_archive' => true,
+        'supports' => array( 'title', 'editor','thumbnail' ),
+        'menu_position' => 20, 
+        'menu_icon' => 'dashicons-admin-customizer',
+	);
+
+	register_post_type( 'portfolio', $args );
+
+    // Déclaration de la Taxonomie
+    $labels = array(
+        'name' => 'Type de projets',
+        'new_item_name' => 'Nom du nouveau Projet',
+    	'parent_item' => 'Type de projet parent',
+    );
+    
+    $args = array( 
+        'labels' => $labels,
+        'public' => true, 
+        'show_in_rest' => true,
+        'hierarchical' => true, 
+    );
+
+    register_taxonomy( 'type-projet', 'portfolio', $args );
+}
+add_action( 'init', 'thomas_register_post_types' ); // Le hook init lance la fonction
